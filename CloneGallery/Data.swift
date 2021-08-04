00b4b3f4f05f02d852ce8047c88f035f17204acd
a.kravcov <a.kravcov@movavi.com>
2021-08-05 02:13:57 +0700

    LocalConfigure

---
+++ CloneGallery/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  CloneGallery
+//
+//  Created by a.kravcov on 05.08.2021.
+//
+
+import UIKit
+
+class Data {
+    let data = [Model(image: UIImage(named: "pepe"),
+                      title: "Мемы",
+                      count: "22")]
+}
---
+++ CloneGallery/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  CloneGallery
+//
+//  Created by a.kravcov on 05.08.2021.
+//
+import UIKit
+
+struct Model {
+    var image: UIImage?
+    var title: String
+    var count: String
+}
