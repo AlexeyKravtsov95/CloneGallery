81192d35838e6e5e39838161713ed22978e19dec
a.kravcov <a.kravcov@movavi.com>
2021-08-05 19:06:44 +0700

    working with cell

--- CloneGallery/Data.swift
+++ CloneGallery/Data.swift
@@ -8,7 +8,46 @@
 import UIKit
 
 class Data {
-    let data = [Model(image: UIImage(named: "pepe"),
+    let data = [
+        "Section0":[Model(image: UIImage(named: "testpicture"),
                       title: "Мемы",
-                      count: "22")]
+                      count: "22"),
+                Model(image: UIImage(named: "testpicture"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "testpicture"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "testpicture"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "pepe"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "pepe"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "pepe"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "pepe"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "pepe"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "pepe"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "pepe"),
+                                  title: "Мемы",
+                                  count: "22"),
+                Model(image: UIImage(named: "pepe"),
+                                  title: "Мемы",
+                                  count: "22")],
+        "Section1": [Model(image: UIImage(named: "pepe"),
+                           title: "Ajnj",
+                           count: "22")]
+    ]
+    
 }
--- CloneGallery/ViewController.swift
+++ CloneGallery/ViewController.swift
@@ -9,31 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private var cell = Data().data
-    
-    private lazy var layout: UICollectionViewFlowLayout = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        
-        return layout
-    }()
-    
-    private lazy var imageCell: UIImageView = {
-        let image = UIImageView()
-        image.contentMode = .scaleAspectFit
-        image.clipsToBounds = true
-        return image
-    }()
-
-    private lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        collectionView.frame = view.bounds
-        collectionView.backgroundColor = .white
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        return collectionView
-    }()
+    private var collectionView = GalleryCollectionView()
     
     private lazy var myAlbumLabel: UILabel = {
         let label = UILabel()
@@ -55,11 +31,11 @@ class ViewController: UIViewController {
         
         title = "Альбомы"
         view.backgroundColor = .white
-        setupLayout()
         setupHierarchy()
+        setupLayout()
     }
     
-    func setupHierarchy() {
+    func setupLayout() {
         myAlbumLabel.translatesAutoresizingMaskIntoConstraints = false
         myAlbumLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
         myAlbumLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
@@ -71,32 +47,13 @@ class ViewController: UIViewController {
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        collectionView.topAnchor.constraint(equalTo: myAlbumLabel.topAnchor, constant: 50).isActive = true
+        collectionView.topAnchor.constraint(equalTo: myAlbumLabel.topAnchor, constant: 30).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
     
-    func setupLayout() {
+    func setupHierarchy() {
         view.addSubview(myAlbumLabel)
         view.addSubview(showAllLabel)
         view.addSubview(collectionView)
     }
 }
-
-extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.contentView.backgroundColor = .green
-
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 150, height: 150)
-    }
-}
-
